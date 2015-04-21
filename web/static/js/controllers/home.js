c4941bdb4cb523af3a86da6668d5ce2bf143c659
Sam Terrell <sam.terrell@perceptivesoftware.com>
2015-04-21 00:16:36 -0400

    Handle no user on landing on game route

--- web/static/js/controllers/home.js
+++ web/static/js/controllers/home.js
@@ -1,10 +1,13 @@
 angular.module('app').controller('HomeCtrl',
-function($scope, $rootScope, $location, $cookies) {
+function($scope, $rootScope, $location, $cookies, $routeParams) {
   $scope.save_name = function(name) {
     if(name && name.trim().length > 0) {
       $cookies.put("name", name);
       $rootScope.name = name;
-      $location.path('/games');
+      if($routeParams.game_id)
+        $location.url('/games/'+encodeURIComponent($routeParams.game_id));
+      else
+        $location.path('/games');
     } else {
       $scope.errors = {name: true};
     }
